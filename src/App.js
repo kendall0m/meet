2784418eed99f9d738624f832b4a726770abc746
kendall0m <marykmiciotto@gmail.com>
2024-04-29 18:02:14 -0400

    filter events by city

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-// src/App.js
-
+import { useEffect, useState } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import NumberOfEvents from './components/NumberOfEvents';
+import { getEvents } from './api';
 import './App.css';
 
+
+
+
+
 const App = () => {
- return (
-   <div className="App">
-     <CitySearch />
-     <EventList />
-   </div>
- );
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  }
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
+  
+  return (
+    <div className="App">
+      <CitySearch />
+      <NumberOfEvents />
+      <EventList events={events} />
+    </div>
+  );
 }
 
 export default App;
